bbe415c9d6c19fbfdd1fca4f218788ebd592995e
Puszek997 <m.golynski66@gmail.com>
2026-07-28 18:49:24 +0200

    match main project flags

--- cmake/CompileFlags.cmake
+++ cmake/CompileFlags.cmake
@@ -5,13 +5,29 @@ if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
         compile_flags
         -pedantic-errors
         -pipe
-        # -fstrict-flex-arrays=3
-        # -fstack-protector-strong
-        # -fno-common
+        -fno-common
+
         # -fno-rtti
         # -fsafe-buffer-usage-suggestions
-        # -fcf-protection=full
-        # -fstack-clash-protection
         # -fno-exceptions
+
+        #ssf
+        -fstrict-flex-arrays=3
+        # -fstack-clash-protection
+        # -fstack-protector-strong
+        # -fcf-protection=full
+
+        # -Wl,-z,nodlopen -Wl,-z,noexecstack \
+        # -Wl,-z,relro -Wl,-z,now \
+        # -Wl,--as-needed -Wl,--no-copy-dt-needed-entries
+        # -D_GLIBCXX_ASSERTIONS \
+        # -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST \
+
+        #redhat
+        # -mshstk
+        # -fsplit-stack
+        # -fstack-limit-register
+        # -fstack-limit-symbol
+        # -fno-stack-array
     )
 endif()
